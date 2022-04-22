386a44c619f5ca3879ac60b2da5f8a1f46196b64
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-04-22 15:16:01 +0430

    Update main.py

--- php-python/main.py
+++ php-python/main.py
@@ -62,7 +62,7 @@ with open ("./Pass-File-List/passlist.txt","r") as file:
         if(i == (lenght - 1)):
             print("cant find password :(")
             sys.exit(0)
-        # send i[index] in password to sender function to Get is Norrect or Not
+        # send i[index] in password to sender function to Get is correct or Not
         main("admin",passfile[i],i,user_input_user)
         i += 1
         
