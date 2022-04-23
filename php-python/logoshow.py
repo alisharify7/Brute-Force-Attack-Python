4f849845b70e5f92fe77ae61a561ec3f6e807d5c
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-04-23 23:31:58 +0430

    Update logoshow.py

--- php-python/logoshow.py
+++ php-python/logoshow.py
@@ -12,3 +12,7 @@ def print_lock():
     :G&&&&&&#B#B#&&&&&#7
     """
     print(logo)
+    print("[Hello world and hello to you] :)\nGithub: [AliSharifyy]\nGithub: [Ali-Moattarirad]\n")
+    print("[] First move Project Folder to your local host Directory")
+    print("Enter your Local host addres:  ")
+    print("example:\n-> http://localhost/php-python/php/index.php\n-> http://127.0.0.1/php-python/php/index.php")
